34761d0c83a875b2b4ef35399d076b73956cbea8
wunjunjia <2562547453@qq.com>
2020-10-05 14:19:26 +0800

    [feature]新增tag组件

---
+++ ui/tag/index.js
@@ -0,0 +1,5 @@
+import ElmTag from './index.vue'
+
+export const Tag = function (Vue) {
+  Vue.component('ElmTag', ElmTag)
+}
---
+++ ui/tag/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <span
+    :style="styleObj"
+    class="elm-tag"
+  >
+    <slot />
+  </span>
+</template>
+
+<script>
+import px2rem from 'material/utils/px2rem'
+
+export default {
+  name: 'ElmTag',
+  props: {
+    color: {
+      type: String,
+      default: '#007fff',
+    },
+    fontSize: {
+      type: Number,
+      default: 24,
+    },
+  },
+  computed: {
+    styleObj() {
+      return {
+        fontSize: px2rem(this.fontSize),
+        color: this.color,
+      }
+    },
+  },
+}
+</script>
